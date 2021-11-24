340abe34eaed9f0e90154c51c1f86041f78dea38
2006309-univesp <90932385+2006309-univesp@users.noreply.github.com>
2021-11-24 00:20:08 -0300

    página de login

--- mobile/eagen/src/screens/Preload/index.js
+++ mobile/eagen/src/screens/Preload/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './style';
-
+import React, { useEffect } from 'react';
+import { Container, LoadingIcon } from './style';
+//import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 import EagenLogo from '../../assets/real-logo.svg';
 
+
 export default () => {
+
+  const navigation = useNavigation();
+
+useEffect(()=>{
+const checkToken = async () => {
+  const token = await AsyncStorage.getItem('token');
+  if(token) {
+    //validar o token
+     }else {
+       navigation.navigate('SignIn');
+}
+}
+checkToken();
+}, []);
+
   return (
     
       <Container>
         <EagenLogo width="100%" height="160" />
+        <LoadingIcon size="large" color="#FFFFFF" />
       </Container>
     
   );
-};
+}
--- mobile/eagen/src/screens/SignIn/index.js
+++ mobile/eagen/src/screens/SignIn/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
-import {Container} from './style';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold 
+} from './style';
+
+import SignInput from '../../components/SignInput';
+
+import EagenLogo from '../../assets/real-logo.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
   return (
     <>
       <Container>
-        <Text>SignIn</Text>
+        <EagenLogo width="100%" height="160"/>
+
+      <InputArea>
+          <SignInput IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"/>
+
+          <SignInput IconSvg={LockIcon}
+          placeholder="Digite sua senha"/>
+
+
+         <CustomButton>
+        <CustomButtonText>LOGIN</CustomButtonText>
+      </CustomButton>
+      
+      </InputArea>
+<SignMessageButton>
+  <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+  <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+</SignMessageButton>
+
       </Container>
     </>
   );
